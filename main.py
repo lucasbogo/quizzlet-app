a05d70be5a45beb6f809cca93b7773ed5b3bdd84
Lucas Abreu Bogo <lucas@lucas-abreu-bogo>
2022-10-31 13:23:44 -0300

    while loop to check number of questions

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from quiz_brain import QuizBrain
 
 question_bank = [] # question bank = a list of question objects | Emptylist = []
 
-for question in question_data: # Loop through each of the questins inside the question_data
-    question_text = question["text"] # for each of this questions, I created a variable called question_text
+for question in question_data: # Loop through each of the questins inside the question_data.
+    question_text = question["text"] # for each of this questions, I created a variable called question_text.
     question_answer = question["answer"]
-    new_question = Question(question_text, question_answer) # this constructor expects two parameters
+    new_question = Question(question_text, question_answer) # this constructor expects two parameters.
     question_bank.append(new_question)
     
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+# Check if the quiz still has questions through a while loop.
+while quiz.still_has_questions():
+    # If there are questions, then go to next one.
+    quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -20,5 +20,7 @@ class QuizBrain:
     
     # Method NextQuestion
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        current_question = self.question_list[self.question_number] # Starts off at zero.
+        # This ensures that question number gets increased everytime we show the user
+        self.question_number += 1
         input(f"Q.{self.question_number}:{current_question.text} (True/False): ")
