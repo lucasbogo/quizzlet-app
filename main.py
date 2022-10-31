2a70694b17cdaaf7d06a445356b1aaa032418bda
Lucas Abreu Bogo <lucas@lucas-abreu-bogo>
2022-10-31 17:26:06 -0300

    UI started

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 
 question_bank = [] # question bank = a list of question objects | Emptylist = []
@@ -10,13 +11,16 @@ for question in question_data: # Loop through each of the questins inside the qu
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer) # this constructor expects two parameters.
     question_bank.append(new_question)
-    
-quiz = QuizBrain(question_bank)
 
-# Check if the quiz still has questions through a while loop
-while quiz.still_has_questions():
+# Create question bank
+quiz = QuizBrain(question_bank)
+# Create Quiz Interface
+quiz_ui = QuizInterface()
+
+# Check if the quiz still has questions through a while loop | commented this, because TKinter might get confused as it runs in a While Loop by default
+""" while quiz.still_has_questions():
     # If True: there are questions, then go to next one. If False, closes the while loop.
-    quiz.next_question()
+    quiz.next_question() """
 
 print(f"You've completed the quiz")
 print(f"your final score was: {quiz.score}/{quiz.question_number}")
--- ui.py
+++ ui.py
@@ -1,1 +1,18 @@
+from tkinter import *  # Import everyrhing from TKinter Module
+
+
+
 THEME_COLOR = "#375362"
+
+class QuizInterface:
+    
+    # This is called everytime we create a new object from this class
+    def __init__(self):
+        # Property window set as a new object from the TK class
+        self.window = Tk()
+        # Change aspects of the window
+        self.window.title("Quizzler")
+        
+        
+        # Set program to run. TK Inter wowrks by running this endless loop It is constantly checking for updates in the UI.
+        self.window.mainloop()
